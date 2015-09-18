c811a230811b9cb637260d1e8087f0413c9557c0
Diana <Diana@Diana-PC>
2015-09-17 23:20:30 -0500

    Se agrega framework boostrap y jquery

--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>pagina de inicio</title>
+        
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        
+        <script src="js/jquery-1.11.3.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="panel panel-primary" style="width:25%; margin: auto;">
+            <div class="panel-heading">
+                <h3>Inicio de Sesion</h3>
+            </div>
+            <div class="panel-body">
+                <table>
+                    <tr>
+                        <td><label>Usuario:</label></td>
+                        <td><input type="text" class="form-control" placeholder="Usuario"/></td>
+                    </tr>
+                    <tr>
+                        <td><label>Contraseña:</label></td>
+                        <td><input type="password" class="form-control" placeholder="Contraseña"/></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </body>
 </html>
