4f3c4a02d505a5027c2f8c60683e605674f23f41
Diana <Diana@Diana-PC>
2015-09-23 23:31:19 -0500

    se implementan iconos y se trabaja en el formulario principal

--- HerramientasClase.php
+++ HerramientasClase.php
@@ -1,8 +1,43 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<html>
+    <head>
+        <title>Herramientas de Clase</title>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <link href="css/general.css" rel="stylesheet" type="text/css"/>
+        <link href="font-awesome-4.4.0/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+        
+        <script src="js/jquery-1.11.3.js" type="text/javascript"></script>
+        <script src="js/bootstrap.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="img/logo.jpg" class="logo" alt=""/>
+                </div>
+                <div class="col-md-8" >
+                    <img src="img/banner-docentes.png" alt="banner" class="img-responsive"/>
+                </div>
+                <div class="col-md-2">
+                    informacion de sesion
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="list-group">
+                        <a class="list-group-item" href="#"><i class="fa fa-home fa-fw"></i>&nbsp; Home</a>
+                        <a class="list-group-item" href="#"><i class="fa fa-book fa-fw"></i>&nbsp; Library</a>
+                        <a class="list-group-item" href="#"><i class="fa fa-pencil fa-fw"></i>&nbsp; Applications</a>
+                        <a class="list-group-item" href="#"><i class="fa fa-cog fa-fw"></i>&nbsp; Settings</a>
+                    </div>
+                </div>
+                <div class="col-md-10">
+                    <div class="panel panel-default">
+                        <h1>Contenido</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
 
